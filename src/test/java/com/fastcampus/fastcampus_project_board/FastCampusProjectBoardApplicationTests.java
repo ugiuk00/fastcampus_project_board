364969d6392260a28ffcb5801d9abca107eb2856
andy <yuniugi1234@gmail.com>
2025-05-06 00:14:37 +0900

    #7 - 스프링 부트 프로젝트 초기화

---
+++ src/test/java/com/fastcampus/fastcampus_project_board/FastCampusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampus_project_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
