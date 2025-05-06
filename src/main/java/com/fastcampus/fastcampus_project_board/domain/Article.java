e1bf8a1e7c2984c353a318ee6f7d0a09c89210e7
andy <yuniugi1234@gmail.com>
2025-05-06 19:50:06 +0900

    #8 - 도메인 클래스 설계(게시글, 댓글)

---
+++ src/main/java/com/fastcampus/fastcampus_project_board/domain/Article.java
@@ -0,0 +1,20 @@
+package com.fastcampus.fastcampus_project_board.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+
+    private Long id;
+    private String title; // 제목
+    private String content; // 본문
+    private String hashtag;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+
+
+
+}
